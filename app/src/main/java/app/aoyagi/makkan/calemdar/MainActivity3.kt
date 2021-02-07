2d4366ed9541ae4d76d430dd9fb9a68194137367
TowaAoyagi <tab.git.kamui@gmail.com>
2021-02-07 14:51:48 +0900

    add comment

--- app/src/main/java/app/aoyagi/makkan/calemdar/MainActivity3.kt
+++ app/src/main/java/app/aoyagi/makkan/calemdar/MainActivity3.kt
@@ -15,18 +15,28 @@ class MainActivity3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
+        //textViewというidのtextView変数を宣言する
         val textView = findViewById<TextView>(R.id.textView)
+        //画面遷移で持ってきたデータの読み込み、dateをキーにして呼び出してtextという変数に格納する
         val text = intent.getStringExtra("date")
+        //SharedPreferencesのインスタンスを生成する
         dataStore = getSharedPreferences("Datastore", Context.MODE_PRIVATE)
+        //SharedPreferencesからさっき作ったtextをキーとしてデータを呼び出してreadという変数に格納
         val read = dataStore.getString(text,"NoData")
+        //textViewにreadの中身をテキストとしてセットする
         textView.text = read
     }
 
     fun save(v: View) {
+        //画面遷移で持ってきたデータの読み込み、dateをキーにして呼び出してtextという変数に格納する
         val text = intent.getStringExtra("date")
+        //変数stringにエディットテキストに入力されているテキストの中身を格納する
         val string = editText.text.toString()
+        //SharedPreferencesに値を収納する為のコード
         val editor = dataStore.edit()
+        //SharedPreferencesにtextをキーとしてstring変数を格納する
         editor.putString(text, string)
+        //変更を適用する
         editor.apply()
     }
 }
